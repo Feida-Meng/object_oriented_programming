fde5a3f21b46330c89ba03fc6873ca852d11b80b
codonculous <feidameng01@gmail.com>
2017-03-29 16:45:37 -0400

    def level_up and collect_treasure method

--- player.rb
+++ player.rb
@@ -5,17 +5,37 @@
 # health_points should start at 10.
 # Your class should have an instance method called level_up that increases lives by one.
 # Your class should have an instance method called collect_treasure that increases gold_coins by one. If gold_coins is a multiple of ten (eg, 10, 20, 30, and so on) then the collect_treasure method should run the level_up method.
-# Your class should have an instance method called do_battle that accepts one damage argument and subtracts it from the player's health_points. If health_points falls below one, subtract one from lives and reset health_points to ten. If you have run out of lives, this method should run another method called restart (see below).
+# Your class should have an instance method called do_battle that accepts one damage argument and
+# subtracts it from the player's health_points. 
+#If health_points falls below one, subtract one from lives and reset health_points to ten.
+#If you have run out of lives, this method should run another method called restart (see below).
 # The restart instance method should set all attributes back to their starting values (5, 0, and 10).
 
 class Player
 
-  def initialize(name, gold_coins, health_points, lives)
+  def initialize(name)
     @name = name
-    @gold_coins = gold_coins
-    @health_points = health_points
-    @lives = lives
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
 
   end
+
+  def level_up
+    @lives += 1
+
+  end
+
+  def collect_treasure
+    @gold_coins += 1
+    if @gold_coins % 10 == 0
+      level_up
+    end
+  end
+
+  def do_battle
+
+  end
+
 
 end
